94a7a514dc55b4f2b00a6ba00f64b4efa4f7230e
Piyush Kumar <kumarpiyush1008@gmail.com>
2018-06-06 11:11:24 +0530

    Added news to database

--- fetch_news_indiatoday/news_api/models.py
+++ fetch_news_indiatoday/news_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class News(models.Model):
+    heading = models.CharField(max_length=255)
+    content = models.TextField()
+    img_src = models.URLField()
+
+    def __str__(self):
+        return self.heading
--- fetch_news_indiatoday/news_api/views.py
+++ fetch_news_indiatoday/news_api/views.py
@@ -1,31 +1,49 @@
 from django.http import JsonResponse
 from lxml import html
 import requests
+from .models import News
+
 
 # Create your views here.
-
-data = {'news': []}
-#   Dictionary so that to save the input in json format, it is made global for further use.
 
 
 def test(request):
     """
     Fetches and returns the news from front page in json format
     """
-    r = requests.get('https://www.indiatoday.in/technology/news')
+    response = requests.get('https://www.indiatoday.in/technology/news')
 
-    tree = html.fromstring(r.content)
+    data = {'news': []}
+    #   Dictionary so that to save the input in json format.
 
-    img_src = tree.xpath('/html/body/div[1]/main/div/section/div[3]/div[1]/div/div/img/@src')
-    headings = tree.xpath('/html/body/div[1]/main/div/section/div[3]/div[1]/div/div/h3/@title')
-    content = tree.xpath('/html/body/div[1]/main/div/section/div[3]/div[1]/div/div/p/text()')
+    if response:
 
-    for i in range(len(headings)):
+        tree = html.fromstring(response.content)
+
+        img_src = tree.xpath('/html/body/div[1]/main/div[1]/section/div[3]/div[1]/div/div/img/@src')
+        headings = tree.xpath('/html/body/div[1]/main/div[1]/section/div[3]/div[1]/div/div[2]/h3/@title')
+        content = tree.xpath('/html/body/div[1]/main/div/section/div[3]/div[1]/div/div/p/text()')
+
+        for i in range(len(img_src)):
+
+            data_exist = News.objects.filter(heading=headings[i])
+
+            if not data_exist:
+                data_save_obj = News.objects.create(heading=headings[i], content=content[i], img_src=img_src[i])
+                data_save_obj.save()
+
+            data['news'].append({
+                'heading': headings[i],
+                'content': content[i],
+                'img_src': img_src[i],
+            })
+
+        return JsonResponse(data)
+
+    else:
+
         data['news'].append({
-            'heading': headings[i],
-            'content': content[i],
-            'img_src': img_src[i]
+            'error': 'There is no response from the request.get() method'
         })
 
-    return JsonResponse(data)
-
+        return JsonResponse(data)
